39cedf4b5738d0a850a228874afa880127e9d927
prastut <kr.prastut@gmail.com>
2019-04-22 14:15:34 +0530

    passing auth to every request

--- testingPackage.js
+++ testingPackage.js
@@ -11,47 +11,49 @@ accel
     console.log(error.response.data);
   });
 
-// accel
-//   .getBalance("0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a")
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+accel
+  .getBalance(auth, "0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a")
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-// accel
-//   .sendTransaction(
-//     "0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a",
-//     "0x8988dcebebe2b2cc753c34fc269e2ae4471e7ea5",
-//     "1000000000000000"
-//   )
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+accel
+  .sendTransaction(
+    auth,
+    "0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a",
+    "0x8988dcebebe2b2cc753c34fc269e2ae4471e7ea5",
+    "1000000000000000"
+  )
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-// accel
-//   .call("e9f37c04edd58fd87e10", "get")
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+accel
+  .call(auth, "e9f37c04edd58fd87e10", "get")
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-// accel
-//   .transaction(
-//     "e9f37c04edd58fd87e10",
-//     "set",
-//     "8",
-//     "0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a"
-//   )
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+accel
+  .transaction(
+    auth,
+    "e9f37c04edd58fd87e10",
+    "set",
+    "8",
+    "0xca5a4ebf81a8faddcdf2932c58c67f6cb8d9a49a"
+  )
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
